9352dbd6051ea798faa61b2a99f24d53f4986826
Hack Reactor Student <student@hackreactor.com>
2017-11-30 12:55:19 -0800

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +14,50 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  var newNode = Tree(value);
+  this.children.push(newNode);
+  
+
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, node) {
+
+  
+  // var node = node || this;
+
+  // if (node.value === target) {
+  //   return true;
+  // }
+  
+  // else if (node.children.length > 0) {
+  //   for (var i = 0; i < node.children.length; i++) {
+  //     return node.contains(target, node.children[i]);
+  //   }
+  // }
+
+  // return false; 
+  var result = false;
+  
+  var check = function(node,target) {
+//    console.log(node);
+    if (node.value === target) {
+      result = true;
+    } 
+    else if(node.children.length > 0) {
+      for (var i = 0; i < node.children.length; i++) {
+         check(node.children[i],target);
+      }
+    }
+  };
+
+  check(this,target);
+
+  return result;
+
 };
 
-
+// Note : this is a basic tree, we dont make any linked nodes here .
 
 /*
  * Complexity: What is the time complexity of the above functions?
