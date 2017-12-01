a5defed3e28b56c8e0ec494805643fcdcd66426b
Hack Reactor Student <student@hackreactor.com>
2017-11-30 21:15:41 -0800

    set complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,81 @@
 var BinarySearchTree = function(value) {
+
+  var bst = new NodeT(value);
+  
+
+  bst.insert = function(value) {
+    bst.insertRecursively(this,value);
+  };
+
+  bst.insertRecursively = function(node,value) {
+    
+    if (value <= node.value) {
+      if (node.left === null) {
+        node.left = new NodeT(value);
+      } else {
+        bst.insertRecursively(node.left,value);
+      }
+    } else if (value > node.value) {
+      if (node.right === null) {
+        node.right = new NodeT(value);
+      } else {
+        bst.insertRecursively(node.right,value);
+      }
+    }
+
+  };
+
+  bst.contains = function(value) {
+
+    var result = false;
+
+    var innerContains = function(node, value) {
+        if (node === null) {
+          result = false;
+        } else if(node.value === value) {
+          result = true;
+        } else if(node.value > value) {
+          innerContains(node.left,value);
+        } else if(node.value < value) {
+          innerContains(node.right,value);
+        }
+    };
+
+  innerContains(this,value);
+
+    return result;
+  };
+
+  
+
+  bst.depthFirstLog = function(callback,node) {
+    
+    
+    if(node !== null) {
+      callback(node.value) ;
+    }
+    
+    if (node.left !== null) {
+      bst.depthFirstLog(callback, node.left);
+    }
+
+    if (node.right !== null) {
+      bst.depthFirstLog(callback, node.right);
+    }
+  
+  };
+
+  return bst;
+
+
 };
 
 
+var NodeT = function(value) {
+  this.left = null;
+  this.right = null;
+  this.value = value;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me//
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+
+  if (!this.contains(item)) {
+    this._storage[item] = item;
+  }
+
 };
 
 setPrototype.contains = function(item) {
+
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
