bf88f91081ca7fe094876e8c4d9676c73c5ead6c
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-12-02 23:48:17 -0800

    completed Hash resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,53 +3,56 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._countEntries = 0; // Use this to resize limit when entries exceed more than 75% of limit
-  //this._resizeLimit = Math.floor(this._limit * 0.75);
+  this._countEntries; // Use this to resize limit when entries exceed more than 75% of limit
 };
+
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-
-  this._countEntries++;
-  if (this._countEntries >= Math.floor(this._limit * 0.75)) {
-    this.resize(this._limit * 2);
-  }
-  
+    
   if (bucket === undefined) {
-    this._storage.set(index, [[k, v]]);
+      this._storage.set(index, [[k, v]]);
+      //this._countEntries++;
   } else {
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucket[i][1] = v;
-      } else {
-        bucket.push([k, v]);
+      for (var i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === k) {
+          bucket[i][1] = v;
+        } else {
+          bucket.push([k, v]);
+          //this._countEntries++;
+        }
       }
-    }
+  }
+
+  this._countEntries = this.count();
+  if (this._countEntries === Math.floor(this._limit * 0.75)) {
+    var newLimit = this._limit * 2;
+    this.resize(newLimit);
   }
   
 };
 
+
 HashTable.prototype.resize = function(newLimit) {
  // make a new temporary storage
-  oldStorage = this._storage;
-  oldLimit = this._limit;
+  var oldStorage = this._storage;
+  var oldLimit = this._limit;
   this._storage = LimitedArray(newLimit);
   this._limit = newLimit;
-  this._countEntries = 0;
-
+ // this._countEntries = 0;
   // get all the elements of current storage & reassign them in the new storage
   for (var i = 0; i < oldLimit; i++) {
     var bucket = oldStorage.get(i);
-    console.log('here');
     if (bucket) {
-      for (var i = 0; i < bucket.length; i++) {
-        this.insert(bucket[i][0], bucket[i][1]);
+      for (var j = 0; j < bucket.length; j++) {   // if loop-control variable 'i' is same in both for loops , its a nightmare, hence came let
+        this.insert(bucket[j][0], bucket[j][1]);
       } 
     }   
   } 
   
 };
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -64,18 +67,19 @@ HashTable.prototype.retrieve = function(k) {
   }
 };
 
+
+
 HashTable.prototype.remove = function(k) {
+
+  this._countEntries = this.count();
+  if (this._countEntries === Math.floor(this._limit * 0.25)) {
+    this.resize(this._limit / 2);
+  }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
 
-  // if (this._countEntries !== 0) {
-  //   this._countEntries--;
-  // }
   
-  // if (this._countEntries <= Math.floor(this._limit * 0.25)) {
-  //   this.resize(this._limit / 2);
-  // }
-
   if(bucket) {
   	for (var i = 0; i < bucket.length; i++) {
       if (bucket[i][0] === k) {
@@ -84,9 +88,23 @@ HashTable.prototype.remove = function(k) {
       }
     }
   }
+
 };
 
 
+
+HashTable.prototype.count = function() {
+
+  var cnt = 0;
+  for (var i = 0; i < this._limit; i++) {
+    var bucket = this._storage.get(i);
+    if (bucket) {
+      cnt += bucket.length;
+    }   
+  } 
+  return cnt;
+
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
